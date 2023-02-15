544f3df71aafb3ea7bc5e22132c93ab47b4eac50
rfigge24 <91072139+rfigge24@users.noreply.github.com>
2023-02-15 13:26:27 +0100

    Update offline.py

--- offline.py
+++ offline.py
@@ -9,7 +9,9 @@ squaresize = 25
 def manualCornerDetection(img, size):
     return ret, corners
 
-def calibrateCamera(size, imagenames):
+# calibrates and saves the camera matrix to a specified file:
+def cameraCalibration(size, imagefnames, outfname):
+    #-----------------------------------Corner finding part----------------------------------------------
     # termination criteria
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
@@ -20,30 +22,39 @@ def calibrateCamera(size, imagenames):
     # scale the objp to the size of the chessboard squares in mm:
     objp = objp * squaresize
 
-    # Arrays to store object points and image points from all the images.
+    # Arrays to store object points and image points from all the images:
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
     
-    for fname in imagenames:
+    for fname in imagefnames:
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
         # Find the chess board corners
         ret, corners = cv.findChessboardCorners(gray, size, None)
     
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            objpoints.append(objp)
-            corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-            imgpoints.append(corners2)
-            # Draw and display the corners
-            cv.drawChessboardCorners(img, size, corners2, ret)
-            cv.imshow('img', img)
-            cv.waitKey(0)
-            
+        if not ret:
+            corners = manualCornerDetection(gray,size)
+            ret = True
+        
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, size, corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(0)
+
     cv.destroyAllWindows()
 
+    #------------------------------------------Calibration part------------------------------------------
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    # writing the camera intrinsic matrix to an XML file for later use:
+    s = cv.FileStorage(outfname, cv.FileStorage_WRITE)
+    s.write('K', mtx)
+    s.release()
 
 
 images = glob.glob('*.jpg')
-calibrateCamera((9,6), images)
+cameraCalibration((9,6), images, "intrinsicmatrix")
